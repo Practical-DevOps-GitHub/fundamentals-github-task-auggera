36b647cdd518e4394831cda1169fc1003d7bd1da
Augustin Gerasym <114820190+auggera@users.noreply.github.com>
2023-10-11 15:58:14 +0300

    Create send-discord-notification.js

---
+++ send-discord-notification.js
@@ -0,0 +1,39 @@
+(function () {
+  const { Octokit } = require("@octokit/core");
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const axios = require("axios");
+
+  async function sendDiscordNotification() {
+    try {
+      const { data } = await octokit.request(
+        "GET /repos/:owner/:repo/pulls/:pull_number",
+        {
+          owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+          repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+          pull_numbers: process.env.GITHUB_EVENT.pull_request.number,
+        },
+      );
+
+      const pullRequest = data;
+      const discordMessage = `New PR opened by ${pullRequest.user.login}: ${pullRequest.html_url}`;
+
+      await axios.post(
+        `https://discordapp.com/channels/1161586183785160816/1161586503407915078`,
+        {
+          content: discordMessage,
+        },
+        {
+          headers: {
+            Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+          },
+        },
+      );
+
+      console.log("Discord notification sent successfully.");
+    } catch (error) {
+      console.log("Error sending Discord notification", error.message);
+    }
+  }
+
+  sendDiscordNotification();
+})();
